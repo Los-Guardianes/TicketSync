95484150b97d361f9da4c994a10cf80ea3b47aab
Rodrigoxd330 <166078608+Rodrigoxd330@users.noreply.github.com>
2025-10-25 12:33:17 -0500

    Optimizando home y filtros de layout
    - Se usa DTOS
    - Se evitan varias llamadas al back

--- backend/src/main/java/com/guardianes/TuTicket/servicioEventos/DTO/CategoriaEventoDTO.java
+++ backend/src/main/java/com/guardianes/TuTicket/servicioEventos/DTO/CategoriaEventoDTO.java
@@ -1,9 +1,21 @@
 package com.guardianes.TuTicket.servicioEventos.DTO;
 
+import com.guardianes.TuTicket.servicioEventos.model.CategoriaEvento;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
+@Data
+@NoArgsConstructor
 public class CategoriaEventoDTO {
-    private String idCategoria;
+    private Integer idCategoria;
     private String nombre;
     private BigDecimal comision;
+
+    public CategoriaEventoDTO(CategoriaEvento categoriaEvento){
+        this.idCategoria =  categoriaEvento.getIdCategoria();
+        this.nombre = categoriaEvento.getNombre();
+        this.comision = categoriaEvento.getComision();
+    }
 }
--- backend/src/main/java/com/guardianes/TuTicket/servicioEventos/DTO/EventoDTO.java
+++ backend/src/main/java/com/guardianes/TuTicket/servicioEventos/DTO/EventoDTO.java
@@ -1,20 +1,49 @@
 package com.guardianes.TuTicket.servicioEventos.DTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.guardianes.TuTicket.servicioEventos.model.Evento;
 import com.guardianes.TuTicket.servicioEventos.model.TipoMoneda;
 import com.guardianes.TuTicket.servicioUbicacion.DTO.CiudadDTO;
 import com.guardianes.TuTicket.servicioUsuarios.DTO.OrganizadorDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventoDTO {
     private Integer idEvento;
     private String nombre;
     private String descripcion;
     private String informaAdic;
     private String restricciones;
-    //URL IMG Y MAPA
+    private String urlImagen;
+    private String urlMapa;
     private String direccion;
-    private TipoMoneda tipoMoneda;
+    private TipoMoneda moneda;
     private Integer maxComprasTickets;
-    private OrganizadorDTO organizadorDTO;
+    private Integer idUsuario; //organizador
+
+    @JsonProperty("ciudad")
     private CiudadDTO ciudadDTO;
+
+    @JsonProperty("categoria")
     private CategoriaEventoDTO categoriaDTO;
+
+    public EventoDTO(Evento evento){
+        this.idEvento = evento.getIdEvento();
+        this.nombre = evento.getNombre();
+        this.descripcion = evento.getDescripcion();
+        this.informaAdic = evento.getInformAdic();
+        this.restricciones = evento.getRestricciones();
+        this.urlImagen = evento.getUrlImagen();
+        this.urlMapa = evento.getUrlMapa();
+        this.direccion = evento.getUrlMapa();
+        this.moneda = evento.getMoneda();
+        this.maxComprasTickets = evento.getMaxComprasTickets();
+        this.idUsuario = evento.getOrganizador().getIdUsuario();
+        this.ciudadDTO = new CiudadDTO(evento.getCiudad());
+        this.categoriaDTO = new CategoriaEventoDTO(evento.getCategoria());
+    }
 }
