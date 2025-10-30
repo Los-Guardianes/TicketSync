dd4bea0f3f961e2057f90ab6191b3df09ede220a
Rodrigoxd330 <166078608+Rodrigoxd330@users.noreply.github.com>
2025-10-29 23:07:52 -0500

    Pdfs de tickets dinámicos

--- backend/src/main/java/com/guardianes/TuTicket/servicioPedidos/service/PDFService.java
+++ backend/src/main/java/com/guardianes/TuTicket/servicioPedidos/service/PDFService.java
@@ -4,8 +4,7 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.guardianes.TuTicket.servicioEventos.model.Evento;
-import com.guardianes.TuTicket.servicioEventos.model.Funcion;
+import com.guardianes.TuTicket.servicioEventos.model.*;
 import com.guardianes.TuTicket.servicioPedidos.model.DetalleCompra;
 import com.guardianes.TuTicket.servicioPedidos.model.OrdenCompra;
 import com.guardianes.TuTicket.servicioPedidos.model.Ticket;
@@ -43,8 +42,6 @@ public class PDFService {
     }
 
     public byte[] generarComprobantePDF(OrdenCompra orden) throws Exception {
-        return null;
-        /*
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         Rectangle customSize = new Rectangle(400, 300);
         Document document = new Document(customSize);
@@ -57,8 +54,7 @@ public class PDFService {
         document.add(logo);
 
         Font tituloFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 16);
-        int numero = new Random().nextInt(100000); // 0 a 99999
-        String numeroFormateado = String.format("%05d", numero);
+        String numeroFormateado = String.format("%05d", orden.getIdOrdenCompra());
         Paragraph titulo = new Paragraph("Comprobante de pago — Nº " + numeroFormateado, tituloFont);
         titulo.setAlignment(Element.ALIGN_CENTER);
         document.add(titulo);
@@ -73,10 +69,10 @@ public class PDFService {
                 .sum();
 
         double total = detalles.stream()
-                    .mapToDouble(detalle -> detalle.getEntrada().getPrecioCalculado().doubleValue() * detalle.getCantidad())
+                    .mapToDouble(detalle -> detalle.getTarifa().getPrecioBase().doubleValue() * detalle.getCantidad())
                     .sum();
 
-        document.add(new Paragraph("Nombre y apellido del usuario: " + user.getNombre() + " " + user.getApellido()));
+        document.add(new Paragraph("Nombre del usuario: " + user.getNombre() + " " + user.getApellido()));
         document.add(new Paragraph("Total de entradas: " + totalTickets));
         document.add(new Paragraph("Precio total: S/ " + total));
 
@@ -91,7 +87,7 @@ public class PDFService {
         return baos.toByteArray();
     }
 
-    public byte[] generarTicketPDF(String codigo) throws Exception {
+    public byte[] generarTicketPDF(Integer idTicket) throws Exception {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
         Rectangle ticketSize = new Rectangle(650, 300);
@@ -103,19 +99,15 @@ public class PDFService {
         Font fontNormal = FontFactory.getFont(FontFactory.HELVETICA, 9);
         Font fontSmall = FontFactory.getFont(FontFactory.HELVETICA, 8);
 
-        Integer idTicket = null;
-        try {
-            idTicket = Integer.parseInt(codigo);
-        } catch (NumberFormatException e) {
-            System.out.println("Código inválido: " + codigo);
-        }
         //Obtener objetos
         Ticket ticket = tktService.getTicketById(idTicket);
         OrdenCompra orden = ticket.getDetalleCompra().getOrdenCompra();
         Usuario user = orden.getUsuario();
         Funcion fc = orden.getFuncion();
         Evento event = fc.getEvento();
-        Entrada entrada = ticket.getDetalleCompra().getEntrada();
+        Tarifa tarifa = ticket.getDetalleCompra().getTarifa();
+        Zona zona = tarifa.getZona();
+        TipoEntrada tipoEntrada = tarifa.getTipoEntrada();
 
         PdfPTable header = new PdfPTable(2);
         header.setWidthPercentage(100);
@@ -128,7 +120,7 @@ public class PDFService {
         logoCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
         header.addCell(logoCell);
 
-        PdfPCell titulo = new PdfPCell(new Phrase("COSTA VERDE, CIRCUITO DE PLAYAS, SAN MIGUEL, PERÚ", fontSmall));
+        PdfPCell titulo = new PdfPCell(new Phrase(event.getDireccion(), fontSmall));
         titulo.setHorizontalAlignment(Element.ALIGN_RIGHT);
         titulo.setBorder(Rectangle.NO_BORDER);
         header.addCell(titulo);
@@ -150,14 +142,14 @@ public class PDFService {
         left.setPadding(8);
 
         left.addElement(new Paragraph(user.getNombre().toUpperCase() + " " + user.getApellido().toUpperCase(), fontNormal));
-        left.addElement(new Paragraph("Tipo de entrada: PREFERENCIAL", fontNormal));
-        left.addElement(new Paragraph("Asiento: PRE-IZQ-C01", fontNormal));
-        left.addElement(new Paragraph("Sector: 203", fontNormal));
-        left.addElement(new Paragraph("Fecha: 02 DE AGOSTO DEL 2025", fontNormal));
-        left.addElement(new Paragraph("Hora: 06:00 P.M.", fontNormal));
-        left.addElement(new Paragraph("Precio: " + entrada.getPrecioCalculado(), fontNormal));
+        left.addElement(new Paragraph("Tipo de entrada: " + tipoEntrada.getNombre(), fontNormal));
+        //left.addElement(new Paragraph("Asiento: PRE-IZQ-C01", fontNormal));
+        left.addElement(new Paragraph("Zona: " + zona.getNombre(), fontNormal));
+        left.addElement(new Paragraph("Fecha: " + fc.getFechaInicio(), fontNormal));
+        left.addElement(new Paragraph("Hora: " + fc.getHoraInicio(), fontNormal));
+        left.addElement(new Paragraph("Precio: " + tarifa.getPrecioBase(), fontNormal));
         left.addElement(new Paragraph("\n* Público en general", fontSmall));
-        left.addElement(new Paragraph("* PROHIBIDO el ingreso con bebidas o alimentos", fontSmall));
+        left.addElement(new Paragraph("* " + event.getRestricciones(), fontSmall));
         mainTable.addCell(left);
 
         PdfPCell dottedLine = new PdfPCell();
@@ -179,10 +171,10 @@ public class PDFService {
         right.setHorizontalAlignment(Element.ALIGN_CENTER);
         right.setVerticalAlignment(Element.ALIGN_MIDDLE);
 
-        Image qr = generarQR(codigo);
+        Image qr = generarQR(idTicket.toString()); //Cambiar a hash
         qr.scaleToFit(90, 90);
         right.addElement(qr);
-        right.addElement(new Paragraph("\n" + entrada.getPrecioCalculado(), fontBold));
+        right.addElement(new Paragraph("\n" + tarifa.getPrecioBase(), fontBold));
 
         mainTable.addCell(right);
         document.add(mainTable);
@@ -199,8 +191,6 @@ public class PDFService {
 
         document.close();
         return baos.toByteArray();
-
-         */
     }
 
 }
