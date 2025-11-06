69617fdaf05c6469db9d93f177162306bbc93322
Rodrigoxd330 <166078608+Rodrigoxd330@users.noreply.github.com>
2025-11-06 10:47:27 -0500

    Usabilidad para el código de descuento

--- frontend/src/components/features/TicketPurchase/components/InfoEventTicket.jsx
+++ frontend/src/components/features/TicketPurchase/components/InfoEventTicket.jsx
@@ -1,6 +1,30 @@
 import "./InfoEventTicket.css"
 
-export const InfoEventTicket = ({evento, totalBruto, montoDescuentoPeriodo, montoDescuentoCodigo, total}) => {
+export const InfoEventTicket = ({evento, totalBruto, montoDescuentoPeriodo, montoDescuentoCodigo, periodo, descuentoCodigo,total}) => {
+
+    const getTipoDescuentoCodigoLabel = () => {
+        if (!descuentoCodigo) return "";
+        if (descuentoCodigo.tipoDesc === "PORCENTAJE") {
+        return `(${descuentoCodigo.valorDescuento}%)`;
+        }
+        if (descuentoCodigo.tipoDesc === "MONTO") {
+        return "(Monto)";
+        }
+        return "";
+    };
+
+        const getTipoDescuentoPeriodoLabel = () => {
+        console.log("Descuento periodo: ",periodo)
+        if (!periodo) return "";
+        if (periodo.tipoDesc === "PORCENTAJE") {
+        return `(${periodo.valorDescuento}%)`;
+        }
+        if (periodo.tipoDesc === "MONTO") {
+        return "(Monto)";
+        }
+        return "";
+    };
+
     return (
         <>
             <h2>{evento?.nombre || 'Cargando...'}</h2>
@@ -13,9 +37,11 @@ export const InfoEventTicket = ({evento, totalBruto, montoDescuentoPeriodo, mont
                 <li>
                     <h3>Total Bruto</h3>
                     <p>S/ {totalBruto || 0}</p>
-                    <h3 >Descuento Periodo</h3>
+                    <h3 >Descuento Periodo {getTipoDescuentoPeriodoLabel()}</h3>
                     <p className="discount">S/ {montoDescuentoPeriodo || 0}</p>
-                    <h3 >Descuento Aplicad</h3>
+                    <h3>
+                        Descuento Aplicado {getTipoDescuentoCodigoLabel()}
+                    </h3>
                     <p className="discount">S/ {montoDescuentoCodigo || 0}</p>
                     <h3>Total</h3>
                     <p>S/ {total || 0}</p>
